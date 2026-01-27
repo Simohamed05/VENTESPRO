36c357d12e716675d319e8bef58fc78a98a6eeb5
Simohamed05 <147811904+Simohamed05@users.noreply.github.com>
2026-01-27 12:07:52 +0100

    Update feature_engineering.py

--- utils/feature_engineering.py
+++ utils/feature_engineering.py
@@ -1,1 +1,15 @@
 
+import pandas as pd
+def create_time_features(df):
+    """Ajoute des variables temporelles aux données."""
+    df["Jour de la semaine"] = df.index.dayofweek
+    df["Mois"] = df.index.month
+    df["Année"] = df.index.year
+    df["Weekend"] = df["Jour de la semaine"].apply(lambda x: 1 if x >= 5 else 0)
+    df["Holiday"] = df["Holiday"].apply(lambda x: 1 if x == "Yes" else 0)
+    return df
+
+def encode_categorical_features(df):
+    """Encode les variables catégoriques pour les modèles et les visualisations."""
+    df = pd.get_dummies(df, columns=["Produit", "Region", "Weather"], drop_first=True)
+    return df
