43d56dcee67559cc514686eeba06bdeff4fb0db9
Simohamed05 <147811904+Simohamed05@users.noreply.github.com>
2026-01-27 12:01:40 +0100

    Update arima_model.py

--- models/arima_model.py
+++ models/arima_model.py
@@ -1,1 +1,7 @@
+from statsmodels.tsa.arima.model import ARIMA
 
+def train_arima(df):
+    """Entraîne un modèle ARIMA pour la prévision des ventes."""
+    model = ARIMA(df['Ventes'], order=(5,1,0))
+    model_fit = model.fit()
+    return model_fit
