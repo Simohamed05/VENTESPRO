765838a5d94504fac1859e5fa6fe8f12c1c60f9d
Simohamed05 <147811904+Simohamed05@users.noreply.github.com>
2026-01-27 12:02:35 +0100

    Update prophet_model.py

--- models/prophet_model.py
+++ models/prophet_model.py
@@ -1,1 +1,10 @@
+from prophet import Prophet
 
+def train_prophet(df):
+    """Entraîne un modèle Prophet."""
+    df_prophet = df.reset_index().rename(columns={"Date": "ds", "Ventes": "y"})
+    model = Prophet()
+    model.add_regressor("Promo")
+    model.add_regressor("Holiday")
+    model.fit(df_prophet)
+    return model
