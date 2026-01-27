89988fa96aa055a83a30fbaec315fa9e0edc8291
Simohamed05 <147811904+Simohamed05@users.noreply.github.com>
2026-01-27 12:23:19 +0100

    Delete arima_model.py

--- arima_model.py
+++
@@ -1,7 +0,0 @@
-from statsmodels.tsa.arima.model import ARIMA
-
-def train_arima(df):
-    """Entraîne un modèle ARIMA pour la prévision des ventes."""
-    model = ARIMA(df['Ventes'], order=(5,1,0))
-    model_fit = model.fit()
-    return model_fit
